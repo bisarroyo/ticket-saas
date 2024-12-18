4fb20ae5282fe8ea6f0f83804ec900b63872d093
Bismark Arroyo <bis.arroyo@gmail.com>
2024-12-18 11:28:28 -0600

    add link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
-import { signOutAction } from '@/app/actions'
+import { signOutAction } from '@/app/actions/authActions'
+import Link from 'next/link'
 
 export default async function PrivatePage() {
   const supabase = await createClient()
@@ -17,6 +18,8 @@ export default async function PrivatePage() {
       <p>Hello {data.user.email}</p>
       <form action={signOutAction}>
         <button type='submit'>Sign out</button>
+        <br />
+        <Link href='/event/123'>Event</Link>
       </form>
     </>
   )
