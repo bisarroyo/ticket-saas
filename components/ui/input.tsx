193dfba17e5db1880a29d48e3edb5688a6a6e7bc
Bismark Arroyo <bis.arroyo@gmail.com>
2024-12-30 19:57:13 -0600

    add calidation and styles for individual components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,16 +1,37 @@
+import { cn } from '@/lib/utils'
+
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
+  id: string
   label: string
   icon?: React.ReactNode
-  error?: boolean
+  error?: { type: string; message: string }[]
 }
 
-const Input: React.FC<InputProps> = ({ label, icon, ...props }) => {
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  icon,
+  error,
+  className,
+  ...props
+}) => {
+  const checkType = (
+    error: { type: string; message: string }[] | undefined,
+    type: string
+  ) => {
+    return error?.some((err) => err.type === type)
+  }
   return (
     <div className='flex flex-col w-full mb-4 relative'>
       <label>
         {label}
         <input
-          className='bg-transparent pl-4 pr-8 py-2 w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50 '
+          id={id}
+          className={cn(
+            'pl-4 pr-8 py-2 w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50',
+            checkType(error, id) && 'border-danger',
+            className
+          )}
           {...props}
         />
       </label>
--- components/ui/password.tsx
+++ components/ui/password.tsx
@@ -1,26 +1,45 @@
 import { useState } from 'react'
 
+import { cn } from '@/lib/utils'
 import { Eye, EyeOff } from 'lucide-react'
 
 interface InputProps extends React.HTMLProps<HTMLInputElement> {
+  id: string
   label: string
-  error?: boolean
+  error?: { type: string; message: string }[]
 }
 
-const InputPassword: React.FC<InputProps> = ({ label, ...props }) => {
+const InputPassword: React.FC<InputProps> = ({
+  id,
+  label,
+  error,
+  className,
+  ...props
+}) => {
   const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const showPasswordField = () => {
     setShowPassword(!showPassword)
+  }
+
+  const checkType = (
+    error: { type: string; message: string }[] | undefined,
+    type: string
+  ) => {
+    return error?.some((err) => err.type === type)
   }
   return (
     <div className='flex flex-col w-full mb-4 relative'>
       <label>
         {label}
         <input
+          id={id}
           type={showPassword ? 'text' : 'password'}
-          min='8'
-          className='bg-transparent pl-4 pr-8 py-2  w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50'
+          className={cn(
+            'bg-transparent pl-4 pr-8 py-2  w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50',
+            checkType(error, id) && 'border-danger',
+            className
+          )}
           {...props}
         />
       </label>
