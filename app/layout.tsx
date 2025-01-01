788db6764a36f204ad1bd0095f858ca59f41591a
Bismark Arroyo <bis.arroyo@gmail.com>
2024-12-31 18:19:46 -0600

    use speed insights component

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
