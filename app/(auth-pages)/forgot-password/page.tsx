ec9a1cb69f269c7c2120706397c536bff14b94dd
Bismark Arroyo <bis.arroyo@gmail.com>
2024-12-31 18:16:33 -0600

    create pages for auth

---
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { useActionState } from 'react'
+import { forgotPassword } from '@/app/actions/authActions'
+
+import Input from '@/components/ui/input'
+import Button from '@/components/ui/button'
+import { Mail } from 'lucide-react'
+
+const Page = () => {
+  const [state, formAction, isPending] = useActionState(forgotPassword, {
+    success: false,
+    error: [{ type: '', message: '' }],
+    inputs: {
+      email: '',
+      fullname: '',
+      password: '',
+      passwordConfirm: ''
+    }
+  })
+  return (
+    <section className='container'>
+      <div className='flex flex-col items-start justify-center min-h-fit py-32 w-full md:max-w-[330px]'>
+        <h1 className='text-3xl'>Recuperar contraseña</h1>
+        <p className='mb-4'>Hola, Bienvenido a Plus Eventos</p>
+        <form
+          action={formAction}
+          className='flex flex-col items-start justify-center w-full'
+        >
+          <Input
+            label='Correo electrónico:'
+            id='email'
+            name='email'
+            type='email'
+            placeholder='nombre@email.com'
+            defaultValue={state?.inputs?.email}
+            required
+            error={state?.error}
+            icon={
+              <Mail
+                strokeWidth={1.5}
+                size='20'
+                color='var(--muted-foreground)'
+                className='absolute bottom-3 end-2'
+              />
+            }
+          />
+          <Button
+            text='Enviar link'
+            textLoading='Enviando...'
+            isPending={isPending}
+            type='submit'
+          />
+        </form>
+        {state?.success && (
+          <p className='text-success mt-4'>
+            Se ha enviado un correo con un link para recuperar tu contraseña
+          </p>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default Page
---
+++ app/(auth-pages)/reset-password/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { useActionState } from 'react'
+import { resetPassword } from '@/app/actions/authActions'
+
+// UI components
+import InputPassword from '@/components/ui/password'
+import Button from '@/components/ui/button'
+
+const Page = () => {
+  const [state, formAction, isPending] = useActionState(resetPassword, {
+    success: false,
+    error: [{ type: '', message: '' }],
+    inputs: {
+      email: '',
+      fullname: '',
+      password: '',
+      passwordConfirm: ''
+    }
+  })
+  return (
+    <section className='container'>
+      <div className='flex flex-col items-start justify-center min-h-fit py-32 w-full md:max-w-[330px]'>
+        <h1 className='text-3xl'>Actualizar contraseña</h1>
+        <p className='mb-4'>Hola, Bienvenido a Plus Eventos</p>
+        <form
+          action={formAction}
+          className='flex flex-col items-start justify-center w-full'
+        >
+          <InputPassword
+            label='Contraseña:'
+            id='password'
+            name='password'
+            placeholder='Ingresa tu contraseña'
+            defaultValue={state?.inputs?.password}
+            required
+            error={state.error}
+          />
+          <InputPassword
+            label='Confirmar contraseña:'
+            id='passwordConfirm'
+            name='passwordConfirm'
+            placeholder='Confirma tu contraseña'
+            defaultValue={state?.inputs?.passwordConfirm}
+            required
+            error={state.error}
+          />
+          <Button
+            text='Actualizar contraseña'
+            textLoading='Actualizando...'
+            isPending={isPending}
+            type='submit'
+          />
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Page
