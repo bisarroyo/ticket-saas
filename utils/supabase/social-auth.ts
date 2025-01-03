b45f5ce96572619583ddcd48838d26154af62f43
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-02 20:49:00 -0600

    add auth util supabase

--- utils/supabase/social-auth.ts
+++ utils/supabase/social-auth.ts
@@ -1,15 +1,13 @@
-import { revalidatePath } from 'next/cache'
 import { Provider } from '@supabase/supabase-js'
 import { createClient } from './client'
 
 export default async function socialAuth(provider: Provider) {
   const supabase = createClient()
   const origin = window.location.origin
-  console.log('origin ' + origin)
   const { error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`
+      redirectTo: `${origin}/`
     }
   })
 
