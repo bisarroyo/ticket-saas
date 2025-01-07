064e9eb0bfe11a21be2bc25cfb61638b8a455982
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-07 17:09:55 -0600

    move page to events

--- app/(events)/page.tsx
+++ app/(events)/page.tsx
@@ -1,16 +1,7 @@
 'use client'
 import Events from '@/components/events/events'
 
-import { eventStore } from '@/app/store/eventStore'
-import { useEffect } from 'react'
-
 export default function Index() {
-  const { fetchData } = eventStore()
-
-  useEffect(() => {
-    fetchData()
-  }, [fetchData])
-
   return (
     <section className=' container my-5'>
       <h1>Próximos eventos</h1>
