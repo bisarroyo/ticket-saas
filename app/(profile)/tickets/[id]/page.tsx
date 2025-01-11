ff4ecbea118f874bc4101cfa0e918220fefc6448
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-11 14:49:14 -0600

    fix event getting info from supabase tickets

--- app/(profile)/tickets/[id]/page.tsx
+++ app/(profile)/tickets/[id]/page.tsx
@@ -1,17 +1,18 @@
 'use client'
 import TicketDetails from '@/components/tickets/ticket-details'
 import Loading from '@/components/ui/loading'
-// import useSingleTicket from '@/hooks/useSingleTicket'
+import useSingleTicket from '@/hooks/useSingleTicket'
 import { useParams } from 'next/navigation'
+
 export default function Page() {
   const params = useParams()
   const { id } = params
-  const eventId = id?.toString() || '1'
 
-  // const { data, loading, error } = useSingleTicket(id as string)
+  const { data: ticket, loading, error } = useSingleTicket(id as string)
 
-  // if (error) return <div>Error</div>
-  const loading = false
+  console.log(ticket)
+
+  if (error || ticket === null) return <div>Error</div>
 
   return (
     <>
@@ -20,15 +21,15 @@ export default function Page() {
       ) : (
         <div className='flex justify-center items-center py-5 '>
           <TicketDetails
-            id={eventId}
-            name='Evento'
+            id={ticket[0].id}
+            name={ticket[0].event_id.name}
             eventDate='2023-01-01'
-            eventLocation='Madrid'
+            eventLocation={ticket[0].event_id.locations[0].name}
             isValid
             ticketPrice={100}
             purshasedDate='2023-01-01'
-            ticketHolderName='Bisarroyo'
-            ticketHolderEmail='bisarroyo@gmail.com'
+            ticketHolderName={ticket[0].user_id.name}
+            ticketHolderEmail={ticket[0].user_id.email}
           />
         </div>
       )}
