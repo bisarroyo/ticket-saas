0a54efba3e8c8bf91a74a1a2f03af03d97c8c1cf
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 16:15:57 -0600

    refactor loading component

--- app/(profile)/tickets/[id]/page.tsx
+++ app/(profile)/tickets/[id]/page.tsx
@@ -3,6 +3,7 @@ import TicketDetails from '@/components/tickets/ticket-details'
 import Loading from '@/components/ui/loading'
 import useSingleTicket from '@/hooks/useSingleTicket'
 import { useParams } from 'next/navigation'
+import { format } from '@formkit/tempo'
 
 export default function Page() {
   const params = useParams()
@@ -10,29 +11,30 @@ export default function Page() {
 
   const { data: ticket, loading, error } = useSingleTicket(id as string)
 
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error || !ticket || ticket.length === 0) {
+    return <div>Error</div>
+  }
   console.log(ticket)
 
-  if (error || ticket === null) return <div>Error</div>
+  const ticketInfo = ticket[0]
 
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className='flex justify-center items-center py-5 '>
-          <TicketDetails
-            id={ticket[0].id}
-            name={ticket[0].event_id.name}
-            eventDate='2023-01-01'
-            eventLocation={ticket[0].event_id.locations[0].name}
-            isValid
-            ticketPrice={100}
-            purshasedDate='2023-01-01'
-            ticketHolderName={ticket[0].user_id.name}
-            ticketHolderEmail={ticket[0].user_id.email}
-          />
-        </div>
-      )}
-    </>
+    <div className='flex justify-center items-center py-5 '>
+      <TicketDetails
+        id={ticketInfo.id}
+        name={ticketInfo.event_id.name}
+        eventDate={format(ticketInfo.event_id.date, { date: 'long' }, 'es')}
+        eventLocation={ticketInfo.event_id.locations[0].name}
+        isValid={ticketInfo.is_active}
+        ticketPrice={100} //need to check the price from db
+        purshasedDate={format(ticketInfo.created_at, { date: 'long' }, 'es')}
+        ticketHolderName={ticketInfo.user_id.name}
+        ticketHolderEmail={ticketInfo.user_id.email}
+      />
+    </div>
   )
 }
