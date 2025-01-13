1ad43cee3178999d1e5eb6cf624d445c766e2536
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 16:14:04 -0600

    fix types for single event

--- hooks/useSingleTicket.tsx
+++ hooks/useSingleTicket.tsx
@@ -3,26 +3,26 @@ import { createClient } from '@/utils/supabase/client'
 import { PostgrestError } from '@supabase/supabase-js'
 
 export default function useSingleTicket(id: string) {
-  const [data, setData] = useState<any>(null)
-  const [error, setError] = useState<PostgrestError | null>(null)
+  const [data, setData] = useState<singleTicketSoldType[]>()
+  const [error, setError] = useState<PostgrestError | null>()
   const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchTickets = async () => {
       setLoading(true)
       const supabase = createClient()
-      const { data, error } = await supabase
+      const { data: ticketData, error: ticketError } = await supabase
         .from('tickets_sold')
         .select(
-          'id, status, created_at,user_id(name, email), event_id(id,name,date,locations(id,name)),  payment_id(ammount)'
+          'id, status, created_at,is_active, user_id(name, email), event_id(id,name,date,locations(id,name)), payment_id(*)'
         )
         .eq('id', id)
         .is('is_active', true)
-      if (error) {
-        setError(error)
+      if (ticketError) {
+        setError(ticketError)
         setLoading(false)
       } else {
-        setData(data)
+        setData(ticketData)
         setLoading(false)
       }
     }
