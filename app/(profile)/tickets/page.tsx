04ba90b6cb187dbff29097d991eca25a812f491c
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 16:13:36 -0600

    format code

--- app/(profile)/tickets/page.tsx
+++ app/(profile)/tickets/page.tsx
@@ -1,14 +1,14 @@
-import Tickets from '@/components/tickets/tickets'
+import Tickets from "@/components/tickets/tickets";
 
 function page() {
   return (
     <>
-      <h1 className='text-center my-5'>Tickets</h1>
-      <div className=' flex justify-center items-center'>
+      <h1 className="text-center my-5 ">Tickets</h1>
+      <div className=" flex justify-center items-center">
         <Tickets />
       </div>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
