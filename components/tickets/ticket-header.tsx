ee733e7bd0efe4f0ee3e300e8c6559efc9188c82
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 16:13:50 -0600

    fix links

--- components/tickets/ticket-header.tsx
+++ components/tickets/ticket-header.tsx
@@ -4,19 +4,19 @@ import Link from 'next/link'
 export default function TicketHeader() {
   return (
     <div className='pb-5 flex justify-between items-center'>
-      <Link href='/dashboard/tickets'>
+      <Link href='/tickets'>
         <p className='flex  items-center gap-2'>
           <ArrowLeft size={20} /> Volver{' '}
           <span className='hidden md:block'>a todos los tickets</span>
         </p>
       </Link>
       <div className='flex justify-between items-center gap-2 md:gap-4'>
-        <Link href='/dashboard/tickets'>
+        <Link href='/tickets'>
           <p className='flex items-center  gap-2'>
             <Download size={20} /> Descargar
           </p>
         </Link>
-        <Link href='/dashboard/tickets'>
+        <Link href='/tickets'>
           <p className='flex items-center gap-2'>
             <Share2 size={20} /> Compartir
           </p>
