e4a1d95a73934fad93627e8afbcff2c5fafd31cb
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 15:07:49 -0600

    fix types for eventStore

--- app/store/eventStore.ts
+++ app/store/eventStore.ts
@@ -2,27 +2,15 @@ import { create } from 'zustand'
 import { createClient } from '@/utils/supabase/client'
 import { PostgrestError } from '@supabase/supabase-js'
 
-// Define el tipo para los datos de los eventos
-interface Event {
-  id: string
-  name: string
-  description: string
-  date: string
-  event_image: string
-  aditional_info?: string
-  locations: {
-    name: string
-  }[]
-}
-
 // Define el estado de Zustand
 interface EventState {
-  data: Event[]
+  data: EventsWithLocationType[]
   loading: boolean
   error: PostgrestError | null
+  user: string
   fetchData: () => Promise<void>
   fetchById: (id: string) => Promise<void>
-  getById: (id: string) => Event | undefined
+  getById: (id: string) => EventsWithLocationType | undefined
 }
 
 // Crea el cliente de Supabase
@@ -33,6 +21,7 @@ export const eventStore = create<EventState>((set, get) => ({
   data: [],
   loading: true,
   error: null,
+  user: '',
 
   fetchData: async () => {
     set({ loading: true, error: null })
@@ -72,8 +61,30 @@ export const eventStore = create<EventState>((set, get) => ({
       }
     }
   },
-  getById: (id: string) => {
+  getById: (id: string): EventsWithLocationType | undefined => {
     const { data } = get() // Obtener el estado actual del store
     return data.find((event) => event.id === id)
+  },
+  getUser: async () => {
+    set({ loading: true })
+
+    const {
+      data: { user },
+      error
+    } = await supabase.auth.getUser()
+
+    set({ user: user?.email })
+
+    if (error) {
+      set({ error: null, loading: false })
+    } else {
+      set({ loading: false })
+    }
+  },
+  updateUser: async () => {
+    const { error } = await supabase.auth.updateUser({})
+    if (error) {
+      console.log(error)
+    }
   }
 }))
