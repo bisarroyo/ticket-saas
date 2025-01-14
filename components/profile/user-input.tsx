d9e2172c58182e32ef9dcbf1c71a39e4269d0cf3
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 18:47:04 -0600

    add an input for user ui

---
+++ components/profile/user-input.tsx
@@ -0,0 +1,44 @@
+import { cn } from '@/lib/utils'
+
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
+  id: string
+  label: string
+  icon?: React.ReactNode
+  error?: string
+}
+
+const InputUserForm: React.FC<InputProps> = ({
+  id,
+  label,
+  icon,
+  error,
+  className,
+  ...props
+}) => {
+  return (
+    <div className='flex flex-col justify-start items-start w-full'>
+      <div className='flex flex-col w-full relative'>
+        <label>
+          {label}
+          <input
+            id={id}
+            className={cn(
+              'pl-4 pr-8 py-2 w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50',
+              error && 'border-danger focus:ring-danger',
+              className
+            )}
+            {...props}
+          />
+        </label>
+        {icon}
+      </div>
+      {error && (
+        <p className='text-red-500 text-sm w-full'>
+          {error && <span>{error}</span>}
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default InputUserForm
