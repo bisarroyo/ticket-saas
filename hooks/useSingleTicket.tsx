cd627f964f6a7e5db4ab87c8220a5ce10019a0dc
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-13 18:47:58 -0600

    fix hook updating minor things

--- hooks/useSingleTicket.tsx
+++ hooks/useSingleTicket.tsx
@@ -3,14 +3,15 @@ import { createClient } from '@/utils/supabase/client'
 import { PostgrestError } from '@supabase/supabase-js'
 
 export default function useSingleTicket(id: string) {
-  const [data, setData] = useState<singleTicketSoldType[]>()
+  const [data, setData] = useState<singleTicketSoldType[] | null>()
   const [error, setError] = useState<PostgrestError | null>()
   const [loading, setLoading] = useState<boolean>(false)
 
+  const supabase = createClient()
   useEffect(() => {
     const fetchTickets = async () => {
+      setError(null)
       setLoading(true)
-      const supabase = createClient()
       const { data: ticketData, error: ticketError } = await supabase
         .from('tickets_sold')
         .select(
@@ -20,15 +21,14 @@ export default function useSingleTicket(id: string) {
         .is('is_active', true)
       if (ticketError) {
         setError(ticketError)
-        setLoading(false)
       } else {
         setData(ticketData)
-        setLoading(false)
       }
+      setLoading(false)
     }
 
     fetchTickets()
-  }, [id])
+  }, [id, supabase])
 
   return { data, error, loading }
 }
