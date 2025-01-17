d78dc25513e3af221e3a71b34c7a98365b66a375
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:16:14 -0600

    add containes class

--- app/(profile)/tickets/page.tsx
+++ app/(profile)/tickets/page.tsx
@@ -1,14 +1,14 @@
-import Tickets from "@/components/tickets/tickets";
+import Tickets from '@/components/tickets/tickets'
 
 function page() {
   return (
-    <>
-      <h1 className="text-center my-5 ">Tickets</h1>
-      <div className=" flex justify-center items-center">
+    <div className='container max-w-xl mx-auto'>
+      <h1 className='text-center my-5 '>Tickets</h1>
+      <div className=' flex justify-center items-center'>
         <Tickets />
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
-export default page;
+export default page
