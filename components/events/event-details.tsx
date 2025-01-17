c839da547e26725da7506041a445fdca375fae18
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:16:54 -0600

    refactor event detauls

--- components/events/event-details.tsx
+++ components/events/event-details.tsx
@@ -1,7 +1,7 @@
 import { format } from '@formkit/tempo'
 
 import Badge from '@/components/ui/badge'
-import { Calendar, CircleCheck, Clock, MapPin } from 'lucide-react'
+import { Calendar, MapPin } from 'lucide-react'
 
 type Props = {
   date: string
@@ -9,40 +9,23 @@ type Props = {
   isAvailable: boolean
 }
 
-export default function EventDetails({ date, location, isAvailable }: Props) {
-  const badgeInfo = [
-    {
-      title: 'Fecha',
-      text: format(date, { date: 'long' }, 'es'),
-      bg: 'bg-slate-100',
-      icon: <Calendar size={16} />
-    },
-    {
-      title: 'Hora',
-      text: format(date, 'h:mm A', 'es'),
-      bg: 'bg-slate-100',
-      icon: <Clock size={16} />
-    },
-    {
-      title: 'Lugar',
-      text: location,
-      bg: 'bg-slate-100',
-      icon: <MapPin size={16} />
-    },
-    {
-      title: 'Tickets',
-      text: isAvailable ? 'Disponible' : 'Agotado',
-      bg: isAvailable
-        ? 'bg-green-200/50 dark:bg-green-700'
-        : 'bg-red-200 dark:bg-red-700',
-      icon: isAvailable ? <CircleCheck size={16} /> : <CircleCheck size={16} />
-    }
-  ]
+export default function EventDetails({ date, location }: Props) {
   return (
-    <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 '>
-      {badgeInfo.map((badge) => (
-        <Badge key={badge.title} {...badge} />
-      ))}
+    <div className='grid gap-4 mb-8'>
+      <Badge
+        title={format(date, 'dddd, D MMMM', 'es')}
+        content={`${format(date, 'HH:mm', 'es')} - ${format(
+          date,
+          'HH:mm',
+          'es'
+        )}`}
+        icon={<Calendar size={40} />}
+      />
+      <Badge
+        title={location}
+        content='San José, CR'
+        icon={<MapPin size={40} />}
+      />
     </div>
   )
 }
