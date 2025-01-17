5f8002b05e28b402151ec24a0878d93de7144649
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:15:50 -0600

    pass props to child

--- app/(profile)/profile/settings/page.tsx
+++ app/(profile)/profile/settings/page.tsx
@@ -2,29 +2,40 @@
 import useUser from '@/hooks/useUser'
 import Loading from '@/components/ui/loading'
 import UserForm from '@/components/profile/user-form'
-import App from '@/components/modal'
+import AppModal from '@/components/modal'
 import PasswordResetForm from '@/components/auth/reset-password'
 
 export default function Page() {
-  const { data, loading } = useUser()
+  const { data, loading, updateUser } = useUser()
 
   return (
-    <div className='max-w-xl mx-auto'>
+    <div className='container max-w-xl mx-auto'>
       {loading ? (
         <Loading />
       ) : (
         <div className='space-y-5'>
-          <div className='p-5 border rounded-lg'>
-            <h2 className=''>Perfil</h2>
+          <div className=' space-y-2'>
+            <h2>Información personal</h2>
             <p>Actualiza tu información persional</p>
-            <UserForm user={data} />
+            <UserForm user={data} loading={loading} updateUser={updateUser} />
           </div>
-          <div className='p-5 border rounded-lg'>
-            <h2 className=''>Seguridad</h2>
+          <hr />
+          <div className=' space-y-2'>
+            <h2>Seguridad</h2>
             <p>Actualiza tus opciones de seguridad</p>
-            <App buttonText='Cambiar contraseña'>
+            <AppModal buttonText='Cambiar contraseña'>
               <PasswordResetForm />
-            </App>
+            </AppModal>
+          </div>
+          <hr />
+          <div className=' space-y-2'>
+            <h2>Eliminar Cuenta</h2>
+            <p>
+              Si no deseas usar nuestra plataforma, puedes eliminar tu cuenta
+            </p>
+            <AppModal buttonText='Eliminar cuenta'>
+              <PasswordResetForm />
+            </AppModal>
           </div>
         </div>
       )}
