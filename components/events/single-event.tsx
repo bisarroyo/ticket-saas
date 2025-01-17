c7d371cc9d422ad3524a48ba91edf7936dfd76b1
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:15:27 -0600

    add new layout for event/id

--- components/events/single-event.tsx
+++ components/events/single-event.tsx
@@ -4,7 +4,6 @@ import EventDescription from './event-description'
 // import EventLocation from './event-location'
 import EventAditionalInfo from './event-aditional-info'
 
-import Button from '@/components/ui/button'
 import Link from 'next/link'
 import EventPrices from './event-prices'
 
@@ -26,24 +25,45 @@ export default function SingleEvent({
   aditional_info: string | undefined
 }) {
   return (
-    <div className=' container'>
-      <div className='max-w-6xl mx-auto rounded-xl overflow-hidden flex flex-row flex-wrap gap-8'>
-        <EventHero img={url} />
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8'>
-          <div className='grid gap-8 w-full'>
-            <EventDescription title={name} description={description} />
+    <div className='container'>
+      <div className='max-w-4xl grid grid-cols-1  gap-x-8 gap-y-12 mx-auto'>
+        <EventHero img={url} isAvailable={true} />
+        <div className='grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-4 lg:gap-8'>
+          <div className='w-full flex flex-col justify-center md:justify-start md:items-start items-center'>
             <EventDetails date={date} location={location} isAvailable={false} />
-            <Link href={`/buy/${id}`} className='w-full'>
-              <Button text='Comprar entradas' />
+            <Link
+              href={`/buy/${id}`}
+              className='bg-primary text-primary-foreground border-primary shadow-xl hover:bg-primary-foreground hover:text-primary text-md py-2 px-4 text-center border rounded-md focus:outline-none transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed'
+            >
+              Comprar Entradas
             </Link>
           </div>
-          <div>
+          <div className='grid gap-8 w-full'>
+            <EventDescription title={name} description={description} />
             <EventPrices />
+            <EventAditionalInfo text={aditional_info} />
           </div>
         </div>
-        <EventAditionalInfo text={aditional_info} />
-        {/* <EventLocation /> */}
       </div>
     </div>
+    // <div className=' container'>
+    //   <div className='max-w-6xl mx-auto rounded-xl overflow-hidden grid grid-cols-1 md:grid-cols-2 gap-8'>
+    //     <EventHero img={url} />
+    //     <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8'>
+    //       <div className='grid gap-8 w-full'>
+    //         <EventDescription title={name} description={description} />
+    //         <EventDetails date={date} location={location} isAvailable={false} />
+    //         <Link href={`/buy/${id}`} className='w-full'>
+    //
+    //         </Link>
+    //       </div>
+    //       <div>
+    //         <EventPrices />
+    //       </div>
+    //     </div>
+    //     <EventAditionalInfo text={aditional_info} />
+    //     {/* <EventLocation /> */}
+    //   </div>
+    // </div>
   )
 }
