4416cea245ec85b2fa7c5e601332f14d4cd391dd
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:12:46 -0600

    temporaly add google permit url

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,13 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
         search: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+        search: ''
       }
     ]
   }
