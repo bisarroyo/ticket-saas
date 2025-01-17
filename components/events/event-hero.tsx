7cdec7bcb700e2084ed52b58e681241dd13a4b20
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-16 18:17:12 -0600

    add available tag to hero

--- components/events/event-hero.tsx
+++ components/events/event-hero.tsx
@@ -1,9 +1,17 @@
 'use client'
 
 import Image from 'next/image'
-export default function EventHero({ img }: { img: string }) {
+
+import { cn } from '@/lib/utils'
+export default function EventHero({
+  img,
+  isAvailable
+}: {
+  img: string
+  isAvailable: boolean
+}) {
   return (
-    <div className='w-full'>
+    <div className='w-full relative'>
       <Image
         src={img}
         priority={true}
@@ -12,6 +20,14 @@ export default function EventHero({ img }: { img: string }) {
         width='1152'
         className='w-full object-cover aspect-[16/7] rounded-2xl transition-opacity duration-500'
       />
+      <p
+        className={cn(
+          'text-md px-4 py-2 w-fit bg-white/70 backdrop-blur-md border border-white/20 rounded-full shadow-lg absolute bottom-4 left-4',
+          isAvailable ? 'text-available' : 'text-danger'
+        )}
+      >
+        {isAvailable ? 'Entradas disponibles' : 'Entradas agotadas'}
+      </p>
     </div>
   )
 }
