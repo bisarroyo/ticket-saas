d2c2773f03798f29831c2ff5af98011ab1d27409
Bismark Arroyo <bis.arroyo@gmail.com>
2025-01-27 22:51:00 -0600

    add correct types from supabase

--- components/events/single-event.tsx
+++ components/events/single-event.tsx
@@ -6,6 +6,7 @@ import EventAditionalInfo from './event-aditional-info'
 
 import Link from 'next/link'
 import EventPrices from './event-prices'
+import { ArrowUpRight } from 'lucide-react'
 
 export default function SingleEvent({
   id,
@@ -14,16 +15,9 @@ export default function SingleEvent({
   date,
   location,
   description,
-  aditional_info
-}: {
-  id: string | undefined
-  name: string
-  url: string
-  date: string
-  location: string
-  description: string
-  aditional_info: string | undefined
-}) {
+  aditional_info,
+  prices
+}: TicketSoldWithLocationType) {
   return (
     <div className='container'>
       <div className='max-w-4xl grid grid-cols-1  gap-x-8 gap-y-12 mx-auto'>
@@ -35,35 +29,28 @@ export default function SingleEvent({
               href={`/buy/${id}`}
               className='bg-primary text-primary-foreground border-primary shadow-xl hover:bg-primary/50 text-md py-2 px-4 text-center border rounded-md w-full focus:outline-none transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed'
             >
-              Comprar Entradas
+              Adquirir entradas
             </Link>
+            <div className='w-full border-t border-white/30 my-5 py-5 '>
+              <p>Organizado por:</p>
+
+              {/* todo: insert correct user */}
+              <Link
+                href={`/profile/${'pluscreative'}`}
+                className='flex items-center gap-1 text-slate-700'
+              >
+                Plus creative <ArrowUpRight size={16} />
+              </Link>
+            </div>
           </div>
           <div className='grid gap-8 w-full'>
             <EventDescription title={name} description={description} />
-            <EventPrices />
-            <EventAditionalInfo text={aditional_info} />
+            <EventPrices prices={prices} />
+            <EventAditionalInfo details={aditional_info} />
+            {/* <EventLocation /> */}
           </div>
         </div>
       </div>
     </div>
-    // <div className=' container'>
-    //   <div className='max-w-6xl mx-auto rounded-xl overflow-hidden grid grid-cols-1 md:grid-cols-2 gap-8'>
-    //     <EventHero img={url} />
-    //     <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8'>
-    //       <div className='grid gap-8 w-full'>
-    //         <EventDescription title={name} description={description} />
-    //         <EventDetails date={date} location={location} isAvailable={false} />
-    //         <Link href={`/buy/${id}`} className='w-full'>
-    //
-    //         </Link>
-    //       </div>
-    //       <div>
-    //         <EventPrices />
-    //       </div>
-    //     </div>
-    //     <EventAditionalInfo text={aditional_info} />
-    //     {/* <EventLocation /> */}
-    //   </div>
-    // </div>
   )
 }
