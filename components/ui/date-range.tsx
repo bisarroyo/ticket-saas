aefbc4e37d473de8921d3e0e40549cb2ecfdca4e
Bismark Arroyo <bis.arroyo@gmail.com>
2025-02-05 16:00:46 -0600

    add component time picker and update styles

--- components/ui/date-range.tsx
+++ components/ui/date-range.tsx
@@ -1,9 +1,14 @@
 import DatePickerComponent from '@/components/ui/date-picker'
+import { Circle, CircleDashed } from 'lucide-react'
 import { useEffect, useState } from 'react'
+import TimePicker from '@/components/ui/time-picker'
 
 const DateRange = () => {
   const [dateStart, setDateStart] = useState<Date>()
   const [dateEnd, setDateEnd] = useState<Date>()
+
+  const [timeStart, setTimeStart] = useState<string>('00:00')
+  const [timeEnd, setTimeEnd] = useState<string>('00:00')
 
   useEffect(() => {
     if (dateEnd && dateStart && dateEnd < dateStart) {
@@ -17,19 +22,41 @@ const DateRange = () => {
     setDateEnd(date)
   }, [])
   return (
-    <>
-      <DatePickerComponent
-        id='date_start'
-        value={dateStart}
-        onChange={(date) => date && setDateStart(date)}
-      />
-      <DatePickerComponent
-        id='date_end'
-        value={dateEnd}
-        minDate={dateStart}
-        onChange={(date) => date && setDateEnd(date)}
-      />
-    </>
+    <div className='flex flex-col bg-white/30 backdrop-blur-md p-2 rounded-lg shadow-lg'>
+      <div className='flex flex-col gap-4 relative'>
+        <div className='absolute top-7 left-1 w-1 h-8 border-r border-dashed border-r-black/50'></div>
+        <div className='flex flex-row gap-4 justify-center items-center'>
+          <Circle size={16} />
+          <p className='w-12'>Inicio</p>
+          <DatePickerComponent
+            id='date_start'
+            value={dateStart}
+            onChange={(date) => date && setDateStart(date)}
+          />
+          <TimePicker
+            id='time_start'
+            value={timeStart}
+            onChange={(time) => time && setTimeStart(time.target.value)}
+          />
+        </div>
+        <div className='flex flex-row gap-4 justify-center items-center'>
+          <CircleDashed size={16} />
+          <p className='w-12'>Fin</p>
+          <DatePickerComponent
+            id='date_end'
+            value={dateEnd}
+            minDate={dateStart}
+            onChange={(date) => date && setDateEnd(date)}
+          />
+          <TimePicker
+            id='time_end'
+            value={timeEnd}
+            minTime={timeStart}
+            onChange={(time) => time && setTimeEnd(time.target.value)}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
