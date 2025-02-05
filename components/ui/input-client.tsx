7ae92cc02ce1a50727ace27befc288cc3496dbf0
Bismark Arroyo <bis.arroyo@gmail.com>
2025-02-05 16:11:08 -0600

    fix text color

--- components/ui/input-client.tsx
+++ components/ui/input-client.tsx
@@ -23,7 +23,7 @@ const InputClient: React.FC<InputProps> = ({
           <input
             id={id}
             className={cn(
-              'pl-4 pr-8 py-2 w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50 text-primary-foreground',
+              'pl-4 pr-8 py-2 w-full border rounded-md focus:ring-1 focus-visible:outline-none focus:outline-none focus:ring-primary-foreground/50 text-foreground',
               error && 'border-danger focus:ring-danger',
               className
             )}
