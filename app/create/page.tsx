42177f3eee072a84693bc4b0b45c9707b542d516
Bismark Arroyo <bis.arroyo@gmail.com>
2025-02-04 23:59:45 -0600

    create page for create event

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+import CreateForm from '@/components/create/create-form'
+
+const Page = () => {
+  return (
+    <div className='container max-w-xl mx-auto my-5'>
+      <CreateForm />
+    </div>
+  )
+}
+
+export default Page
