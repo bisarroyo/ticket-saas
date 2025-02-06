eda61b0594c91c31f945d1ab732f36e97c747a09
Bismark Arroyo <bis.arroyo@gmail.com>
2025-02-06 11:06:10 -0600

    remove google maps package

--- components/events/event-location.tsx
+++ components/events/event-location.tsx
@@ -2,17 +2,12 @@
 //   location: string
 // }
 
-import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps'
-
 export default function EventLocation() {
   const position = { lat: 53.54992, lng: 10.00678 }
   return (
     <div className='w-full h-auto rounded-b-xl overflow-hidden'>
-      <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY!}>
-        <Map defaultCenter={position} defaultZoom={10} mapId='DEMO_MAP_ID'>
-          <AdvancedMarker position={position} />
-        </Map>
-      </APIProvider>
+      <p>{position.lat}</p>
+      <p>{position.lng}</p>
     </div>
   )
 }
