258ce87a404adf4ccc2d53269081ff84a73e078f
Bismark Arroyo <bis.arroyo@gmail.com>
2025-04-02 12:55:17 -0600

    fix types error

--- app/buy/[id]/page.tsx
+++ app/buy/[id]/page.tsx
@@ -1,10 +1,16 @@
 'use client'
+import BuyTickets from '@/components/buy/buy-tickets'
+
 import { useParams } from 'next/navigation'
 const Page = () => {
   const params = useParams()
-  const { id } = params
+  const id = params?.id as string | undefined
 
-  return <section className='container'>Page {id}</section>
+  return (
+    <section className='container'>
+      {id ? <BuyTickets id={id} /> : <p>Error: Invalid or missing ID</p>}
+    </section>
+  )
 }
 
 export default Page
