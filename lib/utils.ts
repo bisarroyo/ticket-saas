cd6c789137c7404cd4535c9d7fbb1e14e2cb7aa3
Bismark Arroyo <bis.arroyo@gmail.com>
2025-04-23 17:39:57 -0600

    add utility fox add time to dates

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function combineDateAndTime(date: Date, time: string): Date {
+  const [hours, minutes] = time.split(':').map(Number)
+  const result = new Date(date)
+  result.setHours(hours)
+  result.setMinutes(minutes)
+  result.setSeconds(0)
+  result.setMilliseconds(0)
+  return result
+}
