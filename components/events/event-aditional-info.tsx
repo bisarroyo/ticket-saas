f2eeb59b74f5ff5f84b9672905a032e64adb4281
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-02 15:53:55 -0600

    fix aditional info loading

--- components/events/event-aditional-info.tsx
+++ components/events/event-aditional-info.tsx
@@ -3,6 +3,9 @@ type Props = {
 }
 
 export default function EventAditionalInfo({ details }: Props) {
+  if (details?.length === 0) {
+    return <></>
+  }
   return (
     <div className=''>
       <h3 className='text-2xl font-semibold'>Información adicional</h3>
