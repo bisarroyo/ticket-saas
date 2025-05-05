0baa2b7e8e8a8795fd26f65ab83230e46b4ebdfd
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-05 17:21:13 -0600

    update login pages to use clerk

---
+++ app/(auth-pages)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+export default function Page() {
+  return (
+    <section className='container'>
+      <div className='flex flex-col items-center justify-center min-h-fit py-5 w-full'>
+        <SignUp />
+      </div>
+    </section>
+  )
+}
