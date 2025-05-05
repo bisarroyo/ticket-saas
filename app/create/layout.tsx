2e868c910d1238960c77dcb75812332a3e680627
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-05 17:22:01 -0600

    add layout for supabase context

---
+++ app/create/layout.tsx
@@ -0,0 +1,25 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import SupabaseProvider from '@/lib/supabase-provider'
+
+export function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='min-h-screen'>
+      <SupabaseProvider>{children}</SupabaseProvider>
+    </div>
+  )
+}
+
+export default async function AppLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const { userId } = await auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  return <ProtectedLayout>{children}</ProtectedLayout>
+}
