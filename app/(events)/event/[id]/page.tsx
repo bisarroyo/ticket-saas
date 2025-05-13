601249a3a42102ad51fffa5165063e834280f4ca
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-13 13:21:16 -0600

    use async component for single id event

--- app/(events)/event/[id]/page.tsx
+++ app/(events)/event/[id]/page.tsx
@@ -1,29 +1,43 @@
-'use client'
-import { useParams } from 'next/navigation'
-import { Suspense } from 'react'
+// 'use client'
+// import { useParams } from 'next/navigation'
+// import { Suspense } from 'react'
 
 import SingleEvent from '@/components/events/single-event'
 import NotFound from '@/components/ui/not-found'
-import Loading from '@/components/ui/loading'
+// import Loading from '@/components/ui/loading'
 
-import useSingleEvent from '@/hooks/useSingleEvent'
+// import useSingleEvent from '@/hooks/useSingleEvent'
 
-export default function Page() {
-  const params = useParams()
-  const id: string | undefined = Array.isArray(params.id)
-    ? params.id[0]
-    : params.id
+import { createClient } from '@/utils/supabase/server'
 
-  const { data: event, loading, error } = useSingleEvent(id)
+export default async function Page({ params }: { params: { id: string } }) {
+  console.log('id', params.id)
+  const { id } = params
 
-  // Manejar errores o casos donde no se encuentre el evento
-  if (loading) {
-    return (
-      <div className='container'>
-        <Loading />
-      </div>
-    )
-  }
+  const supabase = await createClient()
+  const { data: event, error } = await supabase
+    .from('events')
+    .select('*')
+    .eq('id', id)
+    .single()
+  console.log('event', event)
+  console.log('error', error)
+
+  // const params = useParams()
+  // const id: string | undefined = Array.isArray(params.id)
+  //   ? params.id[0]
+  //   : params.id
+
+  // const { data: event, loading, error } = useSingleEvent(id)
+
+  // // Manejar errores o casos donde no se encuentre el evento
+  // if (loading) {
+  //   return (
+  //     <div className='container'>
+  //       <Loading />
+  //     </div>
+  //   )
+  // }
   if (!id || !event || error) {
     return (
       <div className='container'>
@@ -34,20 +48,21 @@ export default function Page() {
 
   // // Renderizar el evento si todo está correcto
   return (
-    <Suspense fallback={<Loading />}>
-      <section className='my-5'>
-        <SingleEvent
-          events={event}
-          id={event.id}
-          name={event.name}
-          url={event.event_image}
-          date={event.date}
-          location={event.locations[0]?.name || 'Ubicación no disponible'}
-          description={event.description}
-          aditional_info={event.aditional_info || 'Sin información adicional'}
-          prices={event.prices}
-        />
-      </section>
-    </Suspense>
+    // <Suspense fallback={<Loading />}>
+    <section className='my-5'>
+      <SingleEvent
+        events={event}
+        id={event.id}
+        name={event.name}
+        url={event.event_image}
+        date={event.date}
+        location={'Ubicación no disponible'}
+        description={event.description}
+        aditional_info={event.aditional_info || 'Sin información adicional'}
+        prices={event.prices}
+      />
+    </section>
+    // </Suspense>
+    // <div>hola</div>
   )
 }
