7df8ad05aaf380fead01e0e7f8d87e1af5161fe5
Bismark Arroyo <bis.arroyo@gmail.com>
2025-05-15 19:23:20 -0600

    update single event to client  component

--- app/(events)/event/[id]/page.tsx
+++ app/(events)/event/[id]/page.tsx
@@ -1,23 +1,39 @@
+'use client'
+
 import SingleEvent from '@/components/events/single-event'
 import NotFound from '@/components/ui/not-found'
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/client'
+import { PostgrestError } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
 
-export default async function Page({
-  params
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const { id } = await params
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params
 
-  const supabase = await createClient()
-  const { data: event, error } = await supabase
-    .from('events')
-    .select('*')
-    .eq('id', id)
-    .single()
-  console.log('event', event)
-  console.log('error', error)
+  const [event, setEvent] = useState<EventsType | null>(null)
+  const [error, setError] = useState<PostgrestError | null>(null)
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      const supabase = createClient()
+      const { data, error } = await supabase
+        .from('events')
+        .select('*')
+        .eq('id', id)
+        .single()
+      if (data) {
+        setEvent(data)
+      }
+      if (error) {
+        setError(error)
+        console.log('Error fetching event:', error)
+      }
+    }
+    if (id) {
+      fetchEvent()
+    }
+  }, [id])
+
   if (!id || !event || error) {
     return (
       <div className='container'>
